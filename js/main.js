fa847c56aafd17d9ca1cb65a82c6dddb5c987ee5
Denys <denysbraun1985@gmail.com>
2018-11-26 20:53:02 +0200

    slider 425px

--- js/main.js
+++ js/main.js
@@ -5,27 +5,30 @@ $(document).ready(function(){
          slidesToShow: 3,
          // centerMode: true,
          responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    arrows: true,
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    prevArrow: '<button type="button" class="arrow-prev"><img src="../img/arrow-prev.png"></button>',
-                    nextArrow: '<button type="button" class="arrow-next"><img src="../img/arrow-next.png"></button>',
-                    autoplay: true,
-                    autoplaySpeed: 2000
+             {
+                 breakpoint: 992,
+                 settings: {
+                     arrows: true,
+                     slidesToShow: 1,
+                     slidesToScroll: 1,
+                     prevArrow: '<button type="button" class="arrow-prev"><img src="../img/arrow-prev.png"></button>',
+                     nextArrow: '<button type="button" class="arrow-next"><img src="../img/arrow-next.png"></button>',
+                     autoplay: true,
+                     autoplaySpeed: 2000
+                 }
+             },
+             {
+                     breakpoint: 450,
+                     settings: {
+                         arrows: false,
+                         slidesToShow: 1,
+                         slidesToScroll: 1,
+                         autoplay: true,
+                         autoplaySpeed: 2000
+                     }
+                 }
 
-
-                 // breakpoint: 425,
-                 // settings: {
-                 //     arrows: false,
-                 //      autoplay: true,
-                 //    autoplaySpeed: 2000,
-                 // }
-                }
-
-            }
                     ]
+
 });
 });
